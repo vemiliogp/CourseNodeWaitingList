5fcd0665fc775f40cba345caa25586df494b4150
Victor E Gonzalez P <vemiliogp@gmail.com>
2022-06-16 18:17:39 +0200

    Initial commit

---
+++ public/js/nuevo-ticket.js
@@ -0,0 +1,23 @@
+// Referencias del HTML
+const lblNuevoTicket = document.querySelector("#lblNuevoTicket");
+const btnCrear = document.querySelector("button");
+
+const socket = io();
+
+socket.on("connect", () => {
+  btnCrear.disabled = false;
+});
+
+socket.on("ultimo-ticket", (ultimo) => {
+  lblNuevoTicket.innerHTML = `Ticket: ${ultimo}`;
+});
+
+socket.on("disconnect", () => {
+  btnCrear.disabled = true;
+});
+
+btnCrear.addEventListener("click", () => {
+  socket.emit("siguiente-ticket", null, (ticket) => {
+    lblNuevoTicket.innerText = ticket;
+  });
+});
